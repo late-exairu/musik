01a73470ffea392db01393f7a80592cc3a2f7b7c
Oleg Shelest <lateexairu@gmail.com>
2023-04-26 18:11:36 +0300

    Fix to gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://late-exairu.github.io/musik/',
   plugins: [react()],
+  build: {
+    outDir: './build'
+  }
 })
