a9a34cb1b56a94ad7e5099a1c68032a1b74402e4
Oleg Shelest <lateexairu@gmail.com>
2023-05-05 09:39:42 +0300

    Fix gallery navigation

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -47,7 +47,7 @@ export default function Testimonials({ title, list }) {
             <div className="swiper-prev swiper-button-prev absolute -left-10 top-40 z-10 hidden h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-white shadow-md lg:flex 2xl:-left-36">
               <Icons className="w-3" icon="sliderArrow" />
             </div>
-            <div className="swiper-prev swiper-button-prev absolute -right-10 top-40 z-10 hidden h-14 w-14 cursor-pointer items-center justify-center rounded-full  bg-white shadow-md lg:flex 2xl:-right-36">
+            <div className="swiper-next swiper-button-prev absolute -right-10 top-40 z-10 hidden h-14 w-14 cursor-pointer items-center justify-center rounded-full  bg-white shadow-md lg:flex 2xl:-right-36">
               <Icons className="w-3 rotate-180" icon="sliderArrow" />
             </div>
             {list.map((item, index) => (
